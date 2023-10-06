ee9c40e6dfec1abd1ec01e6ba058c995b0db3c37
Vitalii-fedorov <fva23322@gmail.com>
2023-10-06 14:15:19 +0300

    Added bg

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import styles from './page.module.css'
+import './page.css'
 
 export default function Home() {
   return (
     <div className="test">
+      <div className="tiles">
+        <span className="tiles__text">
+          <span className="tiles__text1">-WELCOME TO VARMD TILES-</span>
+          <p>
+            <span className="tiles__text2">SECURE YOUR TILE SUPPLIES,
+              <br /> WITH VARMD</span>
+            <br />
+            <span className="tiles__text3">• Ceramic Tiles
+            <br />
+            • Porcelain Tiles
+            <br />
+            • Quality Control
+            <br />
+              • Logistic Support</span>
+          </p>
+        </span>
+      </div>
 
     </div>
-  )
+  );
 }
