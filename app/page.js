86c5ec6bac3f9ad608adc1d444cbb73c9b07a732
roud1 <sh.s.roudi@gmail.com>
2023-10-06 15:29:36 +0300

    1

--- app/page.js
+++ app/page.js
@@ -4,22 +4,21 @@ export default function Home() {
   return (
     <div className="test">
       <div className="tiles">
-        <span className="tiles__text">
-          <span className="tiles__text1">-WELCOME TO VARMD TILES-</span>
-          <p />
-            <span className="tiles__text2">SECURE YOUR TILE SUPPLIES,
-             WITH VARMD</span>
+        <div className="tiles__text">
+          <h1 className="tiles__text1">-WELCOME TO VARMD TILES-</h1>
+            <p className="tiles__text2">SECURE YOUR TILE SUPPLIES,
+             WITH VARMD</p>
 
             <div className="tiles__text3">
-              <ul>
-              <li>Ceramic Tiles</li>
-                <li>Porcelain Tiles</li></ul>
-                <ul><li>Quality Control</li>
-                  <li>Logistic Support</li></ul>
-
+              <ul className='column'>
+                <li>Ceramic Tiles</li>
+                <li className='mt'>Porcelain Tiles</li>
+                <li>Quality Control</li>
+                <li className='mt'>Logistic Support</li>
+              </ul>
             </div>
 
-        </span>
+        </div>
       </div>
     </div>
   );
