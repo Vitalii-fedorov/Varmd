4f6a24cfe8f4cdcc538fa422f9ccda0d5be03795
Vitalii-fedorov <fva23322@gmail.com>
2023-10-09 11:52:11 +0300

    finished Contact Us page

--- app/ContactUs/page.jsx
+++ app/ContactUs/page.jsx
@@ -56,13 +56,51 @@ const ContactUs = () => {
                 </div>
 
             </div>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d649.0213101598232!2d72.50738184264442!3d23.045097060160874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDAyJzQyLjQiTiA3MsKwMzAnMjcuNyJF!5e0!3m2!1sru!2sua!4v1696792650688!5m2!1sru!2sua" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3757.239310817001!2d72.50756630078341!3d23.04491470381694!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b5abc78af39%3A0xf52e076225cf1f08!2sTimes%20Square%202%2C%20Thaltej%2C%20Ahmedabad%2C%20Gujarat%20380054!5e0!3m2!1sru!2sua!4v1696835859542!5m2!1sru!2sua" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
 
-                </iframe>
             </div>
 
             <div className="contactsForm">
                 <img src="/example.png" alt="example"/>
+                <div className="contactsForm-form">
+                    <h2>
+                        For any inquiries, questions or commendations. please fill the form :
+                    </h2>
+                    <br/>
+                    <h3>
+                        Please feel free to get in touch using the form below. We’d love to hear for you.
+                    </h3>
+
+                    <div className="contactsForm-form-fields">
+
+                        <div className="contactsForm-form-fields-info">
+
+                            <div className="contactsForm-form-fields-info-1">
+                                <input placeholder="Name" className="contactsForm-form-fields-info-1-nnea">
+
+                                </input>
+                                <input placeholder="Number" className="contactsForm-form-fields-info-1-nnea">
+
+                                </input>
+                            </div>
+                            <div className="contactsForm-form-fields-info-2">
+                                <input placeholder="Email" className="contactsForm-form-fields-info-2-nnea">
+
+                                </input>
+                                <input placeholder="Address" className="contactsForm-form-fields-info-2-nnea">
+
+                                </input>
+                            </div>
+                        </div>
+
+                        <textarea placeholder="Message" type="text" className="contactsForm-form-fields-message">
+
+                        </textarea>
+
+
+                    </div>
+
+                </div>
             </div>
 
         </div>
